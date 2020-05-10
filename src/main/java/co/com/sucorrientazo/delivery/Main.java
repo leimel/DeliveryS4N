3a657804a015903d4a38a9728351ee0691df0244
Meli <magudelo169@gmail.com>
2020-05-10 01:09:43 -0500

    Initial commit

---
+++ src/main/java/co/com/sucorrientazo/delivery/Main.java
@@ -0,0 +1,9 @@
+package co.com.sucorrientazo.delivery;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Su Corrientazo Delivery");
+    }
+
+}
---
+++ src/test/java/co/com/sucorrientazo/delivery/MainTest.java
@@ -0,0 +1,14 @@
+package co.com.sucorrientazo.delivery;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    Main main = new Main();
+
+    @Test
+    public void shouldExecuteProcess(){
+       main.main();
+
+    }
+}
