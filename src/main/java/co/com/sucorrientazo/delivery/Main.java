f4f3838c4df65d8b46d9b75f1e3ac46da623fe6f
Meli <magudelo169@gmail.com>
2020-05-12 23:46:53 -0500

    Create implementation of service locator pattern to manage dependency injection.

--- src/main/java/co/com/sucorrientazo/delivery/Main.java
+++ src/main/java/co/com/sucorrientazo/delivery/Main.java
@@ -9,6 +9,7 @@ import co.com.sucorrientazo.delivery.services.OutputWriterService;
 import co.com.sucorrientazo.delivery.services.impl.DronDeliveryService;
 import co.com.sucorrientazo.delivery.services.impl.FileInputReaderService;
 import co.com.sucorrientazo.delivery.services.impl.FileOutputWriterService;
+import co.com.sucorrientazo.delivery.services.servicelocator.ServiceLocator;
 
 import java.util.List;
 import java.util.logging.Level;
@@ -18,24 +19,39 @@ public class Main {
     private static final Logger LOGGER = Logger.getLogger("Main");
 
     public static void main(String... args) {
+        ServiceLocator serviceLocator = ServiceLocator.getInstance();
+        initializeAndRegisterServices(serviceLocator);
+
         LOGGER.log(Level.INFO,"Start deliveries..........................................");
-        launchDelivery();
+
+        launchDelivery(serviceLocator);
     }
 
+    public static void launchDelivery(ServiceLocator serviceLocator){
 
-    public static void launchDelivery(){
-        SystemProperties systemProperties = SystemProperties.getInstance();
+        InputReaderService inputReaderService = serviceLocator.getService(InputReaderService.class, "fileInputReaderService");
+        DeliveryService deliveryService = serviceLocator.getService(DeliveryService.class, "dronDeliveryService");
+        OutputWriterService outputWriterService = serviceLocator.getService(OutputWriterService.class, "fileOutputWriterService");
 
-        InputReaderService inputReaderService = new FileInputReaderService(systemProperties);
-        List<DronInput> routesPerDron = inputReaderService.read();
-
-        DeliveryService deliveryService = new DronDeliveryService(new DeliveryDronRunner(systemProperties));//new DeliveryServiceFactory().getDeliveryService("Dron");
-        List<DronOutput> finalPositions = deliveryService.deliver(routesPerDron);
-
-        OutputWriterService outputWriterService = new FileOutputWriterService();
+        List<DronInput> routes = inputReaderService.read();
+        List<DronOutput> finalPositions = deliveryService.deliver(routes);
         outputWriterService.write(finalPositions);
 
         LOGGER.log(Level.INFO,"Finished deliveries..........................................");
+    }
+
+    private static void initializeAndRegisterServices(ServiceLocator serviceLocator) {
+        LOGGER.log(Level.INFO, "Initializing Service Locator");
+        SystemProperties systemProperties = SystemProperties.getInstance();
+        DeliveryDronRunner deliveryDronRunner = new DeliveryDronRunner(systemProperties);
+
+        DeliveryService dronDeliveryService = new DronDeliveryService(deliveryDronRunner);
+        InputReaderService fileInputReaderService = new FileInputReaderService(systemProperties);
+        OutputWriterService fileOutputWriterService = new FileOutputWriterService();
+
+        serviceLocator.registerService(DeliveryService.class, "dronDeliveryService", dronDeliveryService);
+        serviceLocator.registerService(InputReaderService.class, "fileInputReaderService", fileInputReaderService);
+        serviceLocator.registerService(OutputWriterService.class, "fileOutputWriterService", fileOutputWriterService);
     }
 
 }
